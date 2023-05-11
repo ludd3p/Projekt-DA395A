ea62a53284feb6a9108e5293be62bbac3be5e799
lucasstory <62547308+lucasstory@users.noreply.github.com>
2023-05-11 20:18:10 +0200

    Fixed year

--- src/app/season/[year]/[raceId]/page.tsx
+++ src/app/season/[year]/[raceId]/page.tsx
@@ -2,17 +2,20 @@
 
 import { useGlobalContext } from "@/app/context/context";
 import { Race } from "@/app/types/types";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+
 
 export default function Year({ params }: { params: { raceId: string } }) {
   const [races, setRaces] = useState<Race[]>([]);
   const { raceId } = params;
-  const { year } = useGlobalContext();
+  //const { year } = useGlobalContext();
 
+  const year = usePathname().slice(8, 12)
   const id = raceId.slice(5, raceId.length);
 
-  console.log(`http://ergast.com/api/f1/${year}/€${id}/results.json`);
 
+  
   useEffect(() => {
     fetch(`http://ergast.com/api/f1/${year}/${id}/results.json`)
       .then((response) => response.json())
@@ -20,6 +23,7 @@ export default function Year({ params }: { params: { raceId: string } }) {
         console.log(data);
       });
   }, [year, id]);
+  
 
   return (
     <div className="container w-5/6 mx-auto">
