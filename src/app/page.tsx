b9b1e09b9768d52227ce8904ea1d4bbbb03d3047
Tobias la Fleur <tobias.lafleur98@gmail.com>
2023-05-11 15:05:41 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+
+type seasons = {
+  season: string;
+  url: string;
+};
 
 export default function Home() {
+  const [raceYears, setRaceYears] = useState<seasons[]>([]);
+
+  useEffect(() => {
+    fetch("http://ergast.com/api/f1/seasons.json?limit=100")
+      .then((response) => response.json())
+      .then((data) => {
+        setRaceYears(data.MRData.SeasonTable.Seasons);
+      });
+  }, []);
+
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl py-3">Grupp 5</h1>
+      {raceYears.map((season) => {
+        return <h1 key={season.season}>{season.season}</h1>;
+      })}
     </div>
   );
 }
